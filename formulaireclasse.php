f61a0254b9f54d3e6f1437bcf12f8d685f9353f9
ttichet <t.tichet@hotmail.fr>
2016-01-25 17:03:04 +0100

    coucou

---
+++ formulaireclasse.php
@@ -0,0 +1,119 @@
+<html>
+	 
+
+		
+        <head>
+				<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+			<title>FORMULAIRE DE LISTE DE FOURNITURES</title>
+			
+        </head>
+
+        <body align='center'>
+		<div align='center'>
+		
+		<div align='center' id="theader">
+		
+		</div>
+		
+		<div align='center' id="tall">
+		
+		<div align='center' id='ttitre'>
+						<h1>Gestion des classes :</h1>
+						<h2>Jacquie EMICHELE   -   Administrateur</h2>
+					</div>
+		
+		<div align='center' id="menugauche">
+			<div  align='center' id="lienmenu">
+				</br><a href="administrateur.php">Retour</a></br></br>
+			</div>
+			<div  align='center' id="lienmenu">
+				</br><a href="accueil.php">Déconnexion</a></br></br>
+			</div>
+	
+		
+		</div>
+	
+		<div align='center'  id='tcontenupage'>
+			<div align='center'  id='tcontenu'>
+		
+        
+						
+			
+					</br>
+					<table>
+						<tr>
+						<td  align = 'center'>
+						<b>Libéllé: </b>
+						</td>
+						  <td colspan="2"  align = 'center'>
+						<b>Supprimer: </b>
+						 </td>
+						  </tr>
+						  	<tr>
+						<td  align = 'center'>
+						 6ème5  
+						</td>
+				
+						  <td colspan="2"  align = 'center'>
+						<a href="#"><img src='croix_rouge.png'> </a> 
+						 </td>
+						  </tr>
+						  	<tr>
+						<td  align = 'center'>
+					    6ème1
+						</td>
+					
+						 		  <td colspan="2"  align = 'center'>
+						 <a href="#"><img src='croix_rouge.png'> </a>
+						 </td>
+						  </tr>
+						  	<tr>
+						<td  align = 'center'>
+						 6ème2
+						</td>
+						
+						 		  <td colspan="2"  align = 'center'>
+						 <a href="#"><img src='croix_rouge.png'> </a>
+						 </td>
+						</tr>
+				
+						
+								 	</table>
+        
+						</br>
+			</br>
+				
+									
+					
+			
+									
+					<table>
+						<FORM action="#" method="POST">
+							<tr>
+							<td colspan='2'>
+								<h2>Ajouter une classe:</h2>
+							</td>
+							</tr>
+							<tr>
+						
+							
+							<td>
+								Nom: <input type='text' name='di' />
+							</td>
+							<td>
+								<input type='submit' name='valid' value='Ajouter' />
+							</td>
+							</tr>
+			
+						</FORM>
+					</table>
+						
+			
+						</div>
+				</div>
+			</div>
+				</div>
+	
+        <body>
+</html>
